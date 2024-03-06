a128806b58a2cdd2182a375e351ecf47242b26a9
Volodymyr Bodnarchuk <volodimir.bodnarcuk321@gmail.com>
2024-03-06 12:23:24 +0000

    (HW)-user: 'Volodymyr_Bodnarchuk-volodimir.bodnarcuk321@gmail.com', commit time: '2024-03-06 12:23:24'

---
+++ basic_programming/lesson_01/hw_solution/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Volodymyr");
+        System.out.println("Bodnarchuk");
+        System.out.println("28");
+        System.out.println("Nurnberg");
+    }
+}
