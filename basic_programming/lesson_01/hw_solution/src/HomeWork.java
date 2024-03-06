b877716967bac26df5e80dd4303d2126f8f4359b
Pavel Verescako <p.verescako@gmail.com>
2024-03-06 10:05:32 +0000

    (HW)-user: 'Pavel_Verescako-p.verescako@gmail.com', commit time: '2024-03-06 10:05:32'

---
+++ basic_programming/lesson_01/hw_solution/src/HomeWork.java
@@ -0,0 +1,8 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Pavel");
+        System.out.println("Verescako"); //Верещако
+        System.out.println("30");
+        System.out.println("Hamburg");
+    }
+}
