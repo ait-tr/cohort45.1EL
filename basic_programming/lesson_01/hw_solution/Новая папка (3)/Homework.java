fd7c31498c58e984d911728dadf8553c0991f5b9
Artur Semenov <ghost_reider_12@mail.ru>
2024-03-06 14:00:11 +0000

    (HW)-user: 'Artur_Semenov-ghost_reider_12@mail.ru', commit time: '2024-03-06 14:00:11'

---
+++ basic_programming/lesson_01/hw_solution/Новая папка (3)/Homework.java
@@ -0,0 +1,7 @@
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println("My name is Artur Semenvo");
+        System.out.println("My age is 38 years old");
+        System.out.println("I live in Lutherstadt Wittenberg");
+    }
+}
