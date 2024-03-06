8c9e44c2e31ab453e8c82189ec2a21ef77d68345
Kostiantyn Yermak <ermakkostya52@gmail.com>
2024-03-06 14:17:46 +0000

    (HW)-user: 'Kostiantyn_Yermak-ermakkostya52@gmail.com', commit time: '2024-03-06 14:17:46'

---
+++ basic_programming/lesson_01/hw_solution/src/HomeWork1.java
@@ -0,0 +1,8 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Kostiantyn");
+        System.out.println("Yermak");
+        System.out.println("30 years");
+        System.out.println("Hannover");
+    }
+}
