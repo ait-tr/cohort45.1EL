40d8872acae105aad09def76d256fb85e99d065f
Aleksandr Gerstenberger <alex_7377@hotmail.com>
2024-03-06 14:37:01 +0000

    (HW)-user: 'Aleksandr_Gerstenberger-alex_7377@hotmail.com', commit time: '2024-03-06 14:37:01'

---
+++ basic_programming/lesson_01/hw_solution/src/Homework.java
@@ -0,0 +1,8 @@
+public class Homework {
+    public static void main(String[] args) {
+        System.out.printf("Имя: Aleksandr\n");
+        System.out.printf("Фамилия: Gerstenberger\n");
+        System.out.printf("Дата рождения: 07.03.1977\n");
+        System.out.printf("Место проживания: Nettetal ");
+    }
+}
