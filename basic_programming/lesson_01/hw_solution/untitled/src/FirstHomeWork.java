14c6c2150aa7f9353c33051002be3405c60112de
Maxim Nikolus <nikolusmaxim409@gmail.com>
2024-03-06 10:06:15 +0000

    (HW)-user: 'Maxim_Nikolus-nikolusmaxim409@gmail.com', commit time: '2024-03-06 10:06:15'

---
+++ basic_programming/lesson_01/hw_solution/untitled/src/FirstHomeWork.java
@@ -0,0 +1,9 @@
+public class FirstHomeWork {
+    public static void main(String[] args) {
+        System.out.println("Максим");
+        System.out.println("Николусь");
+        System.out.println("19 лет");
+        System.out.println("Штаинфурт");
+        // Урок был усвоен,подача отличная.
+    }
+}
