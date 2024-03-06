6c48c58c5874d35af38167ac52b2849eb80ea278
Konstantin Busch <Konstantin.Busch@web.de>
2024-03-06 11:01:41 +0000

    (HW)-user: 'Konstantin_Busch-Konstantin.Busch@web.de', commit time: '2024-03-06 11:01:41'

---
+++ basic_programming/lesson_01/hw_solution/src/Name.java
@@ -0,0 +1,8 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("Konstantin");
+     System.out.println("Busch");
+     System.out.println("40");
+System.out.println("Sankt Augustin");
+    }
+}
