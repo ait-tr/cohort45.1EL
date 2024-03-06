4bf7837d9432aac7a067f8d9177b8b23f3f592cb
Artur Dementyev <artur29072005@yandex.com>
2024-03-06 18:19:20 +0000

    (HW)-user: 'Artur_Dementyev-artur29072005@yandex.com', commit time: '2024-03-06 18:19:20'

---
+++ basic_programming/lesson_01/hw_solution/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Artur");
+        System.out.println("Dementyev");
+        System.out.println("18");
+        System.out.println("Braunschweig");
+    }
+    
+}
