cfc29626b2dfa34b94d37ebf249a464dadcedc43
Pavel Verescako <p.verescako@gmail.com>
2024-03-07 08:05:48 +0000

    (HW)-user: 'Pavel_Verescako-p.verescako@gmail.com', commit time: '2024-03-07 08:05:48'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Приветствуем в CurrencyConverter!");
+        System.out.println();
+        System.out.println("1. EURO");
+        System.out.println("2. USD");
+        System.out.println("3. TL");
+    }
+}
