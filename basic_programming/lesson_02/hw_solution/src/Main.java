001a8469b11805e89340dcffa60218e4c5b57a5f
Aleksandr Gerstenberger <alex_7377@hotmail.com>
2024-03-07 19:20:34 +0000

    (HW)-user: 'Aleksandr_Gerstenberger-alex_7377@hotmail.com', commit time: '2024-03-07 19:20:34'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Приветствуем в CurrencyConverter!\n");
+        System.out.printf("Выберите исходную валюту:\n");
+        System.out.printf("1. EURO\n");
+        System.out.printf("2. USD\n");
+        System.out.printf("3. TL");
+    }
+}
