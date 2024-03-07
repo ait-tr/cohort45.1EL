1d324834ba88e001c0b758f1650bbcc307038cf2
Anastassia Miroshnitsenko <anastasiya_m97@bk.ru>
2024-03-07 18:45:18 +0000

    (HW)-user: 'Anastassia_Miroshnitsenko-anastasiya_m97@bk.ru', commit time: '2024-03-07 18:45:18'

---
+++ basic_programming/lesson_02/hw_solution/work/MainClass.java
@@ -0,0 +1,13 @@
+package cc.work;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Приветствуем в Currency Converter!");
+        System.out.println();
+        System.out.println("Выберите исходную валюту:");
+        System.out.println("1. EURO");
+        System.out.println("2. USD");
+        System.out.println("3. TL");
+
+    }
+}
