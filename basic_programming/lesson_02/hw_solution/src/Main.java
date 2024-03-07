ccfb554823a961b00cc95f5c0eb248e2de041d8f
Aleksandr Gerstenberger <alex_7377@hotmail.com>
2024-03-07 19:31:26 +0000

    (HW)-user: 'Aleksandr_Gerstenberger-alex_7377@hotmail.com', commit time: '2024-03-07 19:31:26'

--- basic_programming/lesson_02/hw_solution/src/Main.java
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Приветствуем в CurrencyConverter!\n");
-        System.out.printf("Выберите исходную валюту:\n");
-        System.out.printf("1. EURO\n");
-        System.out.printf("2. USD\n");
-        System.out.printf("3. TL");
+        System.out.printf("Приветствуем в CurrencyConverter!\n\nВыберите исходную валюту:\n1. EURO\n2. USD\n3. TL");
+
     }
 }
