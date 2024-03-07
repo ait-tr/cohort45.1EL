40e55788e9e04ed744c42d9ac1723499fb82fb7e
Artur Dementyev <artur29072005@yandex.com>
2024-03-07 17:23:13 +0000

    (HW)-user: 'Artur_Dementyev-artur29072005@yandex.com', commit time: '2024-03-07 17:23:13'

---
+++ basic_programming/lesson_02/hw_solution/classwork/CurrencyConverter.java
@@ -0,0 +1,14 @@
+package lesson02.classwork;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+System.out.println("EURO");
+System.out.println("USD");
+System.out.println("TL");
+    }
+
+
+
+
+
+}
