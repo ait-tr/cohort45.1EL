99782e38808bb55c6230a61dc37027804180e304
Kostiantyn Yermak <ermakkostya52@gmail.com>
2024-03-07 18:02:04 +0000

    (HW)-user: 'Kostiantyn_Yermak-ermakkostya52@gmail.com', commit time: '2024-03-07 18:02:04'

---
+++ basic_programming/lesson_02/hw_solution/CurrencyConverter/Mainclass.java
@@ -0,0 +1,12 @@
+package Homework2.CurrencyConverter;
+
+public class Mainclass {
+    public static void main(String[] args) {
+        System.out.println("Приветствуем в CurrencyConverter!");
+
+        System.out.println("Выберите исходную валюту:");
+        System.out.println("1. EURO");
+        System.out.println("2. USD");
+        System.out.println("3. TL");
+    }
+}
