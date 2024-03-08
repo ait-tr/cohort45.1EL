1fa0560ee29966d3453ce0282bfb623cebaf1840
Victor Tcaci <victor.tcaci@yahoo.com>
2024-03-08 11:31:08 +0000

    (HW)-user: 'Victor_Tcaci-victor.tcaci@yahoo.com', commit time: '2024-03-08 11:31:08'

---
+++ basic_programming/lesson_01/hw_solution/src/Number1.java
@@ -0,0 +1,8 @@
+public class Number1 {
+    public static void main(String[] args) {
+        System.out.println("Victor");
+        System.out.println("Tcaci");
+        System.out.println("33");
+        System.out.println("Hagenow");
+    }
+}
