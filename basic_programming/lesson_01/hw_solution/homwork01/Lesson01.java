7b6e05e2c0c4bb532b7c74a360004f20e21f33ee
Karina Humm <karina301297@gmail.com>
2024-03-09 13:06:01 +0000

    (HW)-user: 'Karina_Humm-karina301297@gmail.com', commit time: '2024-03-09 13:06:01'

---
+++ basic_programming/lesson_01/hw_solution/homwork01/Lesson01.java
@@ -0,0 +1,10 @@
+package homwork01;
+
+public class Main{
+    public static void main (String[] args) {
+    System.out.println("Karina");
+    System.out.println("Humm");
+       System.out.println("26");
+         System.out.println("Niederbayern");
+         {
+                }
