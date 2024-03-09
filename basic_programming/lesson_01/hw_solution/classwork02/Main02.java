2b45a1956f815d9edc9292568bc0ad8dd614e469
Vadim Danilchenko <danwadim@gmail.com>
2024-03-09 19:42:18 +0000

    (HW)-user: 'Vadim_Danilchenko-danwadim@gmail.com', commit time: '2024-03-09 19:42:18'

---
+++ basic_programming/lesson_01/hw_solution/classwork02/Main02.java
@@ -0,0 +1,7 @@
+package classwork02;
+
+public class Main02 {
+    public static void main(String[] args) {
+        int number = 5/6;
+    }
+}
---
+++ basic_programming/lesson_01/hw_solution/classwork02/homework.java
@@ -0,0 +1,7 @@
+package classwork02;
+
+public class homework {
+    public static void main(String[] args) {
+        System.out.println("Vadim Danilchenko 42 Dinslaken");
+    }
+}
