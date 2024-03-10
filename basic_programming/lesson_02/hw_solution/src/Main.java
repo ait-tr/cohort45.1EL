030fe2c6bda0bd0f071bf20cd2959a497132d99c
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-03-10 12:43:52 +0000

    (HW)-user: 'Anton_Okhrimenko-antony.okhrimenko@gmail.com', commit time: '2024-03-10 12:43:52'

---
+++ basic_programming/lesson_02/hw_solution/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Приветствуем в CurrencyConverter!\n");
+        System.out.println("Выберите исходную валюту:");
+        System.out.println("1. EURO");
+        System.out.println("2. USD");
+        System.out.println("3. TL");
+
+        // Далее будет Scanner и прочие радости :)
+
+
+    }
+}
