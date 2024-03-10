cab384ddd78d8ebc66eedd514346a028b81858ee
Volha Kulaha <olga.kulaga@gmail.com>
2024-03-10 15:24:10 +0000

    (HW)-user: 'Volha_Kulaha-olga.kulaga@gmail.com', commit time: '2024-03-10 15:24:10'

---
+++ basic_programming/lesson_01/hw_solution/Lesson01/src/HomeTask01.java
@@ -0,0 +1,9 @@
+
+public class HomeTask01{
+    public static void main(String[] args) {
+        System.out.println("Olga Kulaga");
+        System.out.println("vozrast");
+        System.out.println("München,Bayern");
+    }
+
+}
