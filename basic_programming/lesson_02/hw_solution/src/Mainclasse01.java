383bd806c4c1cfe5987da7a10522c64fb3b748b7
Konstantin Busch <Konstantin.Busch@web.de>
2024-03-11 13:36:25 +0000

    (HW)-user: 'Konstantin_Busch-Konstantin.Busch@web.de', commit time: '2024-03-11 13:36:25'

---
+++ basic_programming/lesson_02/hw_solution/src/Mainclasse01.java
@@ -0,0 +1,9 @@
+public class Mainclasse01 {
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+        System.out.println("The is currencyconverter");
+        System.out.println("Euro");
+        System.out.println("USD");
+        System.out.println("TL");
+    }
+}
