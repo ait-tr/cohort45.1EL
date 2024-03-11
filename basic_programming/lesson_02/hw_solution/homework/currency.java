4ab3fcbbe4deab2b8a9a95e22cf72491904aa833
Volha Kulaha <olga.kulaga@gmail.com>
2024-03-11 17:28:20 +0000

    (HW)-user: 'Volha_Kulaha-olga.kulaga@gmail.com', commit time: '2024-03-11 17:28:20'

---
+++ basic_programming/lesson_02/hw_solution/homework/currency.java
@@ -0,0 +1,13 @@
+package lesson02.homework;
+
+public class currency {
+    public static void main(String[] args) {
+        System.out.println("Приветствуем в CurrencyConverter!\n");
+        System.out.println("Выберите исходную валюту:");
+        System.out.println("1.Euro");
+        System.out.println("2.USD");
+        System.out.println("3.TL");
+
+
+    }
+}
