41732c9cdf9a3d058b0de164f57e082e1340bdae
Aleksandr Gerstenberger <alex_7377@hotmail.com>
2024-03-12 20:06:52 +0000

    (HW)-user: 'Aleksandr_Gerstenberger-alex_7377@hotmail.com', commit time: '2024-03-12 20:06:52'

---
+++ basic_programming/lesson_03/hw_solution/src/Homework03.java
@@ -0,0 +1,13 @@
+public class Homework03 {
+    public static void main(String[] args) {
+        long number = 777L;
+        System.out.println(number + "\n");
+        String specification1 = "Переменная \"long\" \n 1. Хранит целое число \n от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807 \n и занимает 8 byte.";
+        System.out.println(specification1);
+        String specification2 = " 2. Литерал в переменной \"long\" записывается в конце с суффиксом \"L\" : 777L.";
+        System.out.println(specification2);
+        String specification3 = " 3. Литералы переменной \"long\" кроме десятиричного \n также можно записывать в бинарном или шестнадцатеричном варианте.";
+        System.out.println(specification3);
+
+    }
+}
