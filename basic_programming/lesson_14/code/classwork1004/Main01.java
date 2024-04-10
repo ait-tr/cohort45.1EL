11f3a9a13f586fe344cd9f984bbb5ea72d26504e
Illia Tkachov <i.o.tkachov@gmail.com>
2024-04-11 01:27:21 +0300

    add lesson 14

---
+++ basic_programming/lesson_14/code/classwork1004/Main01.java
@@ -0,0 +1,8 @@
+package classwork1004;
+
+public class Main01 {
+    public static void main(String[] args) {
+        String s = "Hello world"; // new line
+        System.out.println(s.length());
+    }
+}
